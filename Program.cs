35882fb9c4ba24123128cbc8017cf5a23cf8eaf8
Lenovo <Lenovo@DESKTOP-IA8U2M9>
2019-07-14 11:20:48 +0800

    Stagedddddd

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate
+            Room room = new Room();
+            room.HotelInfo();
+            decimal rate = room.HotelRate();
+            Console.Write("Room rate: " + rate.ToString());
+            Console.ReadKey();
+        }
+        
+
+    }
+}
---
+++ Room.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    public class Room
+    {
+        //Data Type
+        public string RoomName { get; set; }
+        public decimal Rate { get; set; }
+        public int Bed { get; set; }
+
+        public void HotelInfo()
+        {
+            Console.Write("Hello Po!");
+            Console.ReadKey();
+        }
+
+        public decimal HotelRate()
+        {
+            Rate = 2000.50m;
+            return Rate;
+        }
+
+        public string SayMyName(string name)
+        {
+            return name;
+        }
+    }
+}
